9a15281219e20980ed046bc05b12efab0e5c4a94
William Cao <153825457@qq.com>
2019-06-05 17:57:59 +0800

    delete logo

--- mock/index.js
+++ mock/index.js
@@ -1,4 +1,8 @@
 
 const Mock = require('mockjs')
+
+// 正则匹配接口后面的参数
+// Mock.mock(RegExp('url' + '.*'), 'get', require('./json/userInfo'))
+
 //格式： Mock.mock( url, post/get , 返回的数据)；
 Mock.mock('/user/userInfo', 'get', require('./json/userInfo'))
